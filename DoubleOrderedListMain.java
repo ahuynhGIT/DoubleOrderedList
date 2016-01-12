d67e388401498dbcdbac3ab8436da09f01c0468b
Alex Huynh <ahuynh647@mail.com>
2016-01-12 17:14:41 -0500

    Create DoubleOrderedListMain.java

---
+++ DoubleOrderedListMain.java
@@ -0,0 +1,16 @@
+public class DoubleOrderedListMain
+{
+	public static void main(String [] args)
+	{
+		DoubleOrderedList list = new DoubleOrderedList();
+		list.add("Snake",30);
+		list.add("Zebra",10);
+		list.add("Cat",40);
+		list.add("Dog",25);
+		System.out.println(list);
+		System.out.println(list.printAmount());
+		list.delete("Dog");
+		System.out.println(list);
+		System.out.println(list.printAmount());
+	}
+}
